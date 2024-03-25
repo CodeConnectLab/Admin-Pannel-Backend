ebd75471ba5651efaf420377d5fc2cde68423515
Code Connect <164360564+CodeConnectLab@users.noreply.github.com>
2024-03-25 19:28:22 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const mongoose= require("mongoose");
 
 // DB_URI='mongodb://0.0.0.0:27017/Hosting';
     
-DB_URI='mongodb+srv://abhilekhk6:rSl1VKEbcga3ejtA@cluster0.pjn2izj.mongodb.net/merntask?retryWrites=true&w=majority';
+DB_URI='mongodb+srv://codeconnectlabs:3NPknFJTgbNw9ppa@cluster0.ocxugzh.mongodb.net/adminpanelbackend?retryWrites=true&w=majority';
  
 const connectDatabase =()=>{ 
    mongoose.connect(DB_URI,{ 
